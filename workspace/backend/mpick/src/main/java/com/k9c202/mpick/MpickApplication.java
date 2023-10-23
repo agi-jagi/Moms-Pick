a5945fc7c6b88446791ba959b1ff3bd56a008bc8
권준일 <wnsdlf925@naver.com>
2023-10-23 15:38:43 +0900

    chore: basic setting

---
+++ workspace/backend/mpick/src/main/java/com/k9c202/mpick/MpickApplication.java
@@ -0,0 +1,13 @@
+package com.k9c202.mpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpickApplication.class, args);
+	}
+
+}
