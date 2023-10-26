393c366d1386519badb5a32613b112e35055b531
최찬석 <ckstjr3592@naver.com>
2023-10-26 10:01:59 +0900

    chore: 프론트 초기 폴더 구조 생성 및 라이브러리 설치

--- workspace/frontend/mpick/tailwind.config.ts
+++ workspace/frontend/mpick/tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = {
+const config: Config = withMT({
+  corePlugins: {
+    preflight: true,
+  },
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@material-tailwind/react/theme/components**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        sm: "443px", // Change this to your desired value for small screens (sm)
+        md: "660px", // Change this to your desired value for medium screens (md)
+        lg: "800px", // Change this to your desired value for large screens (lg)
       },
     },
   },
   plugins: [],
-}
-export default config
+});
+export default config;
