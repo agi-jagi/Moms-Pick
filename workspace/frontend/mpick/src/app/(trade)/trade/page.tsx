e062fffcf0b250a683804f09178d2280a34c6518
방상제 <sangje112@naver.com>
2023-10-27 12:02:51 +0900

    fix: Zustand Store Persist 적용 디버깅 진행 중

--- workspace/frontend/mpick/src/app/(trade)/trade/page.tsx
+++ workspace/frontend/mpick/src/app/(trade)/trade/page.tsx
@@ -1,9 +1,36 @@
+'use client';
 
+import useStore from "../../../store/useStore";
+import { useTradeStore } from "@/store/tradeStore";
+import { Button } from "@nextui-org/react"
+import { useEffect } from "react";
 
 export default function Trade() {
+
+  const { postId, setPostId, postTitle, setPostTitle } = useTradeStore();
+
+  useEffect(() => {
+    if (typeof window != 'undefined') {
+      const res = localStorage.getItem('postId');
+      console.log(res)
+      // setPostId(res);
+
+    }
+  },[])
+
+  
+
   return (
     <div>
       Trade 페이지
+      <br />
+      {postId}
+      <br />
+      {postTitle}
+      <br />
+      <Button onClick={() => setPostId(30)}>postId 변경</Button>
+
+
     </div>
   );
 }
