b4d477d138fff10c54191ff1050615ca68998eb9
정호윤 <jhy1812@naver.com>
2023-10-30 04:04:55 +0900

    feat: trade repository

--- workspace/backend/mpick/src/main/java/com/k9c202/mpick/trade/repository/TradeRepository.java
+++ workspace/backend/mpick/src/main/java/com/k9c202/mpick/trade/repository/TradeRepository.java
@@ -1,4 +1,18 @@
 package com.k9c202.mpick.trade.repository;
 
+import com.k9c202.mpick.trade.entity.Trade;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
 public interface TradeRepository {
+
+    void save(Trade trade);
+
+    Trade findOne(UUID id);
+
+    List<Trade> findAll();
+
+    List<Trade> findByTitle(String title);
 }
