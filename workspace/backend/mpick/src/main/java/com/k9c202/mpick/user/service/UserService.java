0e4dc168a9b695404a6d9d8d19d8d3077f23ec16
서지현 <gyg03134@naver.com>
2023-10-31 15:51:03 +0900

    feat: 이메일 인증 및 검증

--- workspace/backend/mpick/src/main/java/com/k9c202/mpick/user/service/UserService.java
+++ workspace/backend/mpick/src/main/java/com/k9c202/mpick/user/service/UserService.java
@@ -111,7 +111,7 @@ public class UserService {
     //email 관련
     public void sendCodeToEmail(String toEmail) {
         this.checkDuplicatedEmail(toEmail);
-        String title = "Travel with me 이메일 인증 번호";
+        String title = "맘스픽 이메일 인증 번호";
         String authCode = this.createCode();
         mailService.sendEmail(toEmail, title, authCode);
         // 이메일 인증 요청 시 인증 번호 Redis에 저장 ( key = "AuthCode " + Email / value = AuthCode )
