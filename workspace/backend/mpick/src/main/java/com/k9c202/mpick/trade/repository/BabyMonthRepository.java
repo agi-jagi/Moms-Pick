7393e7a0c012bb3d9eaf5db92d1a1b7894981cda
정호윤 <jhy1812@naver.com>
2023-11-03 14:41:20 +0900

    fix: 엔티티 수정

--- workspace/backend/mpick/src/main/java/com/k9c202/mpick/trade/repository/BabyMonthRepository.java
+++ workspace/backend/mpick/src/main/java/com/k9c202/mpick/trade/repository/BabyMonthRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface BabyMonthRepository extends JpaRepository<BabyMonth, Integer> {
 
-    BabyMonth findByStartMonth(Integer startMonth);
+    Optional<BabyMonth> findByStartMonth(Integer startMonth);
 }
