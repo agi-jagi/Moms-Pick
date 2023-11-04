faff5646ef864bec53976971aab71b7be34c204d
정호윤 <jhy1812@naver.com>
2023-11-04 23:25:35 +0900

    fix: 찜 기능 수정

--- workspace/backend/mpick/src/main/java/com/k9c202/mpick/trade/entity/Wish.java
+++ workspace/backend/mpick/src/main/java/com/k9c202/mpick/trade/entity/Wish.java
@@ -2,7 +2,9 @@ package com.k9c202.mpick.trade.entity;
 
 import com.k9c202.mpick.trade.entity.Trade;
 import com.k9c202.mpick.user.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -12,6 +14,8 @@ import java.sql.Timestamp;
 @Entity
 @Getter
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Wish {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
