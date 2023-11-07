acc6d01fcb22753ba0cd5ce86c57f31145a38b13
권준일 <wnsdlf925@naver.com>
2023-11-07 17:35:17 +0900

    feat: ES connect

--- workspace/backend/mpick/src/main/java/com/k9c202/mpick/elateicSearch/repository/ESTrade.java
+++ workspace/backend/mpick/src/main/java/com/k9c202/mpick/elateicSearch/repository/ESTrade.java
@@ -7,10 +7,12 @@ import lombok.experimental.SuperBuilder;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
 
 import javax.persistence.Id;
 
 @Document(indexName = "trade")
+@Setting(settingPath = "../esJson/trade-setting.json")
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
@@ -23,5 +25,26 @@ public class ESTrade {
 
     @Field(type = FieldType.Text)
     private String title;
+
+    @Field(type = FieldType.Keyword)
+    private String img;
+
+    @Field(type = FieldType.Keyword)
+    private String price;
+
+    @Field(type = FieldType.Keyword)
+    private String latitude  ;
+
+    @Field(type = FieldType.Keyword)
+    private String longitude;
+
+
+    /*
+    이미지
+    가격
+    판매자 닉네임
+    대분류
+
+    */
 
 }
