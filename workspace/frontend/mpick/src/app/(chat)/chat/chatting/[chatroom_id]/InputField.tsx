e020a02802ff631523386078c9c2a35bcfae570f
최찬석 <ckstjr3592@naver.com>
2023-11-08 15:57:38 +0900

    feat: 채팅 기능 구현

--- workspace/frontend/mpick/src/app/(chat)/chat/chatting/[chatroom_id]/InputField.tsx
+++ workspace/frontend/mpick/src/app/(chat)/chat/chatting/[chatroom_id]/InputField.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { useEffect } from "react";
 import { Input } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
-import socket from "./Server";
 // import "./InputField.css";
 
+let socket: any;
 const InputField = (props: any) => {
+  useEffect(() => {
+    const jwt = localStorage.getItem("accessToken");
+    socket = new WebSocket("ws://localhost:3000/ws?jwt=" + jwt);
+    socket.onopen = (e: any) => {
+      console.log("connected", e);
+    };
+    socket.onmessage = (e: any) => {
+      props.chattingReload();
+      console.log("message", e.data.message);
+    };
+  }, []);
+
   return (
     <div
       className="input-area"
@@ -21,19 +33,19 @@ const InputField = (props: any) => {
         style={{ display: "flex", justifyContent: "space-between", width: "100%" }}
       >
         <Input
-          placeholder="Type in here…"
+          placeholder="메시지를 입력해주세요"
           value={props.message}
           onChange={(event) => props.setMessage(event.target.value)}
           style={{ width: "100%", height: "100%" }}
         />
         <Button
           onClick={() => {
-            console.log(props.message);
+            props.setMessage("");
             const data = {
-              chatRoomId : 1,
-              message : props.message
-            }
-            socket.send(JSON.stringify(data))
+              chatRoomId: props.chatRoomId,
+              message: props.message,
+            };
+            socket.send(JSON.stringify(data));
           }}
           disabled={props.message === ""}
           className="send-button"
