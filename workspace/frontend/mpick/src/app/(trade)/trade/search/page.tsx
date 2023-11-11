a51b71e96f1f7accd051e72b6423455bcea51367
sangje Home <sangje112@gmail.com>
2023-11-11 17:31:42 +0900

    feat: Elastic Search 확인 작업중

--- workspace/frontend/mpick/src/app/(trade)/trade/search/page.tsx
+++ workspace/frontend/mpick/src/app/(trade)/trade/search/page.tsx
@@ -12,20 +12,21 @@ import { BsChevronDown } from "react-icons/bs";
 import { BiSolidMessageSquareAdd } from "react-icons/bi";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import axios from "axios";
+import { on } from "events";
 
 
 export default function Search() {
 
   const {isOpen, onOpen, onOpenChange} = useDisclosure();
-  const [selected대분류, setSelected대분류] = useState<string[]>([]);
+  const [filter대분류, setFilter대분류] = useState<string>('');
 
   const [중분류open, set중분류Open] = useState(false);
   const handleOpen중분류 = () => set중분류Open(!중분류open);
-  const [selected중분류, setSelected중분류] = useState<string[]>([]);
+  const [filter중분류, setFilter중분류] = useState<string>('');
 
   const [개월수open, set개월수Open] = useState(false);
   const handleOpen개월수 = () => set개월수Open(!개월수open);
-  const [selected개월, setSelected개월] = useState<string[]>([]);
+  const [filter개월, setFilter개월] = useState<number[]>([]);
 
   const [ 등록open, set등록Open] = useState(false);
   const handleOpen등록 = () => set등록Open(!등록open);
@@ -72,6 +73,15 @@ export default function Search() {
       setSelectedMonthList((prevList) => [...prevList, index]);
     }
   };
+
+  const handleFilter개월 = (index: number) => {
+    // 이미 선택된 항목이라면 제거, 아니라면 추가
+    if (filter개월.includes(index)) {
+      setFilter개월((prevList) => prevList.filter((item) => item !== index));
+    } else {
+      setFilter개월((prevList) => [...prevList, index]);
+    }
+  }
   
 
   const list = [
@@ -145,6 +155,48 @@ export default function Search() {
     console.log(err);
   };
   };
+
+  // ElasticSearch 검색 요청 함수
+  async function searchTrade() {
+    
+    const ElasticURL = "http://k9c202.p.ssafy.io:9200/mpick/_search";
+
+    const data = {
+      query: {
+        bool: {
+          must: [
+            { match: { status: "판매중" }},
+            { match: { tradeMonth: "3" }},
+            { match: { mainCategory: "유모차" }}
+          ],
+          filter: {
+            get_distance: {
+              distance: "100000km",
+              location: {
+                lat: 35.2026038557392,
+                lon: 126.815091346254
+              }
+            }
+          }
+        }
+      },
+      size: 10,
+      from: 0
+    }
+
+    try {
+
+      const res = await axios.get(ElasticURL, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   
   // getCategory 함수를 useEffect 내에서 호출
   useEffect(() => {
@@ -171,6 +223,7 @@ export default function Search() {
     <>
       <div>
       <Button onClick={()=>console.log(categoryList)}>리스트 확인</Button>
+      <Button onClick={searchTrade}>ES 서치 확인</Button>
       <div className="flex gap-4 mt-4 justify-center">
       <Chip
         startContent={<FilterIcon />}
@@ -189,7 +242,7 @@ export default function Search() {
         color="default"
         endContent={<BsChevronDown className="mr-1"/>}
         className="shadow-md"
-        onClick={() => handleOpen중분류()}
+        onClick={() => onOpen()}
       >
         중분류
       </Chip>
@@ -199,7 +252,7 @@ export default function Search() {
         color="default"
         endContent={<BsChevronDown className="mr-1"/>}
         className="shadow-md"
-        onClick={() => handleOpen개월수()}
+        onClick={() => onOpen()}
       >
         개월수
       </Chip>
@@ -218,8 +271,9 @@ export default function Search() {
         >
           판매글 등록
         </Button>
-        <Modal isOpen={등록open} onOpenChange={handleOpen등록} >
-          
+
+        {/* 판매글 등록 입력 폼 모달 */}
+        <Modal isOpen={등록open} onOpenChange={handleOpen등록} >  
     <ModalContent>
           {() => (
             <>
@@ -298,6 +352,8 @@ export default function Search() {
             )}
             </ModalContent>
           </Modal>
+
+          {/* 검색 결과 리스트 */}
       </div>
     </div>
     <div className="mt-5 gap-2 grid grid-cols-2 sm:grid-cols-4">
@@ -321,40 +377,73 @@ export default function Search() {
       ))}
       
     </div>
-    {/* 대분류 카테고리 모달 */}
+
+
+    {/* 필터링 카테고리 모달 */}
     <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
     <ModalContent>
-          {(onClose) => (
+    {() => (
             <>
-              <ModalHeader className="flex flex-col gap-1">카테고리</ModalHeader>
-              <ModalBody>
-              <div className="flex flex-col gap-3">
-      <CheckboxGroup
-        label="대분류"
-        color="primary"
-        value={selected대분류}
-        onValueChange={setSelected대분류}
-      >
-        <Checkbox value="유모차">유모차</Checkbox>
-        <Checkbox value="수유용품">수유용품</Checkbox>
-        <Checkbox value="기저귀">기저귀</Checkbox>
-      </CheckboxGroup>
-      <p className="text-default-500 text-small">선택됨: {selected대분류.join(", ")}</p>
-    </div>
+                {/* <form onSubmit={(e) => registerTrade(e)}> */}
+                <ModalBody>
+                  <Select
+                  label="대분류 선택"
+                  placeholder={filter대분류}
+                  >
+                    {mainCategoryList.map((item, index) => (
+                      <SelectItem key={index} onClick={() => setFilter대분류(item)}>{item}</SelectItem>
+                    ))}
+                  </Select>
+
+                  <Select
+                  label="중분류 선택"
+                  placeholder={filter중분류}
+                  >
+                    {filter대분류 &&
+                      categoryList[filter대분류]?.map((item :string) => (
+                        <SelectItem key={item} onClick={() => setSelectedSubCategory(item)}>{item}</SelectItem>
+                      ))}
+                  </Select>
+                  <p className="font-semibold">
+                    개월 선택 (임산부 이외 중복 가능)
+                  </p>
+                  <div className="flex flex-wrap gap-3">
+                    
+                  {babyMonthList.map((month, index) => (
+                    <Checkbox
+                      key={index}
+                      value={month}
+                      checked={filter개월.includes(index)}
+                      onChange={() => handleFilter개월(index + 1)}
+                      isDisabled={
+                        (index !== 0 && filter개월.includes(1)) ||
+                        (index === 0 && filter개월.some((item) => item !== 1)) // "임산부"가 아닌 다른 개월 선택 시 "임산부" 비활성화
+                      }
+                    >
+                      {month}
+                    </Checkbox>
+                  ))}
+                  </div>
+
+                  <Button onClick={()=>console.log(filter개월)}>담긴 개월 조회</Button>
+                
               </ModalBody>
               <ModalFooter>
-                <Button className="bg-[#5E9FF2] text-white" onPress={onClose}>
+                <Button color="primary" type="submit" 
+                onClick={onOpenChange}
+                >
                   적용하기
                 </Button>
               </ModalFooter>
+              {/* </form> */}
             </>
-          )}
+            )}
         </ModalContent>
 
     </Modal>
 
     {/* 중분류 카테고리 모달 */}
-    <Modal isOpen={중분류open} onOpenChange={handleOpen중분류} >
+    {/* <Modal isOpen={중분류open} onOpenChange={handleOpen중분류} >
     <ModalContent>
           {() => (
             <>
@@ -383,9 +472,9 @@ export default function Search() {
           )}
         </ModalContent>
 
-    </Modal>
+    </Modal> */}
     {/* 개월 카테고리 모달 */}
-    <Modal isOpen={개월수open} onOpenChange={handleOpen개월수} >
+    {/* <Modal isOpen={개월수open} onOpenChange={handleOpen개월수} >
     <ModalContent>
           {() => (
             <>
@@ -414,7 +503,7 @@ export default function Search() {
           )}
         </ModalContent>
 
-    </Modal>
+    </Modal> */}
     </>
   )
 }
