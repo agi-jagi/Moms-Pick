a6c8c099c7fdda18d83a431430a77257c39877b1
정호윤 <jhy1812@naver.com>
2023-11-14 09:42:56 +0900

    feat: 판매완료 기능 구현

--- workspace/backend/mpick/src/main/java/com/k9c202/mpick/trade/controller/request/TradeCompleteRequest.java
+++ workspace/backend/mpick/src/main/java/com/k9c202/mpick/trade/controller/request/TradeCompleteRequest.java
@@ -1,13 +1,18 @@
 package com.k9c202.mpick.trade.controller.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 public class TradeCompleteRequest {
 
-    private Long tradeId;
+    private Long chatRoomId;
 
-    private Long ChatRoomId;
+    public TradeCompleteRequest(Long chatRoomId) {
+        this.chatRoomId = chatRoomId;
+    }
 }
