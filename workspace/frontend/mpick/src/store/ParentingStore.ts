445307b1a809bdf7a7b4b90e477c617cdf0e25b9
최지수 <cjjss11@naver.com>
2023-11-14 17:20:41 +0900

    feat: 식단 정보 axios 통신

--- workspace/frontend/mpick/src/store/ParentingStore.ts
+++ workspace/frontend/mpick/src/store/ParentingStore.ts
@@ -3,9 +3,13 @@ import { create } from "zustand";
 interface Parentingstate {
   parenting: string;
   setParenting: (parenting: string) => void;
+  foodId: number;
+  setFoodId: (foodId: number) => void;
 }
 
 export const useParentingStore = create<Parentingstate>((set) => ({
   parenting: "",
   setParenting: (parenting: string) => set({ parenting }),
+  foodId: 0,
+  setFoodId: (foodId: number) => set({ foodId }),
 }));
