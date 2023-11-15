02b9aecbc3abeefe42de2c8b9cb89aca96346ae6
정호윤 <jhy1812@naver.com>
2023-11-15 09:03:08 +0900

    feat: notfound exception

--- workspace/backend/mpick/src/main/java/com/k9c202/mpick/global/response/NotFoundException.java
+++ workspace/backend/mpick/src/main/java/com/k9c202/mpick/global/response/NotFoundException.java
@@ -1,2 +1,10 @@
-package com.k9c202.mpick.global.response;public class NotFoundException {
+package com.k9c202.mpick.global.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class NotFoundException extends ResponseStatusException {
+    public NotFoundException(String reason) {
+        super(HttpStatus.NOT_FOUND, reason);
+    }
 }
