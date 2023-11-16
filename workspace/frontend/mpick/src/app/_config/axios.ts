79bb1b3f90828943bb8e1ca2eddb8faa614434b2
최찬석 <ckstjr3592@naver.com>
2023-11-16 16:21:34 +0900

    feat: 토큰 에러시 로그인화면으로

--- workspace/frontend/mpick/src/app/_config/axios.ts
+++ workspace/frontend/mpick/src/app/_config/axios.ts
@@ -11,11 +11,46 @@ instance.interceptors.request.use(
     const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
+    } else {
+      localStorage.clear();
+      window.location.href = "/";
+      return Promise.reject();
     }
     return config;
   },
   (error) => {
     // 요청 전에 에러가 발생한 경우 처리하는 로직을 구현합니다.
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터 추가
+instance.interceptors.response.use(
+  (response) => {
+    // 응답을 받은 후 처리해야 할 작업을 수행
+    return response;
+  },
+  (error) => {
+    const originalRequest = error.config;
+    // 토큰이 만료되었거나 유효하지 않은 경우에만 토큰을 재발급 받을 수 있도록 조건을 설정
+    if (error.response.status === 403) {
+      // 여기서 토큰을 재발급 받는 작업을 수행
+
+      alert("세션이 만료되어 로그아웃 되었습니다");
+      localStorage.clear();
+      window.location.href = "/";
+      return Promise.reject(error);
+    } else if (error.response.status === 500) {
+      alert("세션이 만료되어 로그아웃 되었습니다");
+      localStorage.clear();
+      window.location.href = "/";
+      return Promise.reject(error);
+    } else if (error.response.status === 401) {
+      alert("세션이 만료되어 로그아웃 되었습니다");
+      localStorage.clear();
+      window.location.href = "/";
+      return Promise.reject(error);
+    }
     return Promise.reject(error);
   }
 );
